0661710fbc8d371d63a6f70fccfb1febdd681486
waterdoze <anqduong2003@gmail.com>
2023-01-28 14:19:45 -0600

    set up server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 3001
+const uri = process.env.ATLAS_URI
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.set('strictQuery', false)
+mongoose.connect(uri)
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
