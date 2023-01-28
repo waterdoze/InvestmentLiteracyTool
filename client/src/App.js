6a7c11e240d872ac1ee094fb3912ad75ef2347fd
Steven Mao <steven2003mao@gmail.com>
2023-01-28 15:39:58 -0600

    Add Static Components and Minor CSS Additions

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
+import React from "react";
+import './index.css'
+import Title from "./components/Title";
+import Resources from "./components/Resources";
 
 function App() {
-  return (
-    <div className="App">
-        blah
-    </div>
-  );
+	return (
+		<div className="App">
+			<div id="SidePane">
+				<Title />
+			</div>
+			<div id="StockPane">
+
+			</div>
+		</div>
+	);
 }
 
 export default App;
