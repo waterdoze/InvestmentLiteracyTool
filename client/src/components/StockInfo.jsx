bdf22e739d7f945e5d2dfc8254cc65dd048a3eea
Steven Mao <steven2003mao@gmail.com>
2023-01-28 23:44:48 -0600

    Added More Information to StockInfo

--- client/src/components/StockInfo.jsx
+++ client/src/components/StockInfo.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-function StockInfo({price}) {
-    
+function StockInfo({price, oldStock}) {
     return (
         <div className="stockinfo-component">
             <h1 className="stock-name">JPSE</h1>
             <h2 className="stock-price">{price} USD</h2>
+            {price >= oldStock ?
+                <h3 className="stock-difference">
+                    {`+ ${(price - oldStock).toFixed(2)} (${((price - oldStock) / oldStock * 100).toFixed(2)}%) `} 
+                    <i className="fa-solid fa-arrow-up" /></h3>
+                : <h3 className="stock-difference">
+                    {`- ${(oldStock - price).toFixed(2)} (${((price - oldStock) / oldStock * 100).toFixed(2)}%) `} 
+                    <i className="fa-solid fa-arrow-up" /></h3>
+            }
         </div>
     )
 }
