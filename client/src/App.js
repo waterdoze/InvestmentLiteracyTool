98322c8a2d617c2963acd193387e4d66f77f71a4
Kevin <kevin923.dai@gmail.com>
2023-01-28 18:54:41 -0600

    iodk

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-import { useEffect, useState } from 'react'
-=======
 import React, { useEffect, useState } from "react";
 import './index.css'
 import Title from "./components/Title";
@@ -8,48 +5,11 @@ import StockInfo from "./components/StockInfo";
 import Balance from "./components/Balance";
 import Resources from "./components/Resources";
 import Transaction from "./components/Transaction";
->>>>>>> b5af5783a4e9d3470edd66f0216e4b4cd218c94b
 
 import {Data} from './utils/Data';
 import Graph from './components/Graph';
 function App() {
 
-<<<<<<< HEAD
-    const [pastDates, setPastDates] = useState([])
-
-    useEffect( () => {
-        const fetchData = async () => {
-            const response = await fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=META&apikey=TX6O5JWMEMBV5M0M');
-            const data = await response.json();
-
-            const pd = Object.keys(data["Time Series (Daily)"]).map((key) => (
-                {Date: key, Price: parseFloat(data["Time Series (Daily)"][key]["1. open"])}
-            ))
-    
-            setPastDates(pd)
-        } 
-
-        fetchData()
-
-    }, [])
-
-
-    useEffect(() => {
-        console.log(pastDates)
-    }, [pastDates])
-
-    return (
-        <div className="App">
-            {pastDates ? pastDates.map((item, i) => (
-                <div key={i}>Date: {item.Date}, Price: {item.Price} </div>
-
-            ))
-             : <h1>can't show</h1>}
-
-        </div>
-        
-    );
-=======
   const [data, setData] = useState([]);
   const [currData, setCurrData] = useState([]);
   const [stockIndex, setStockIndex] = useState(10);
@@ -103,7 +63,6 @@ function App() {
 			</div>
 		</div>
 	);
->>>>>>> b5af5783a4e9d3470edd66f0216e4b4cd218c94b
 }
 
 export default App;
